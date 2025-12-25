f45ec4333dfff7598883d0a300b19623d5067c75
hukang <13954138926@163.com>
2025-12-25 22:05:38 +0800

    流程

--- src/main/java/com/yy/framework/flowable/handler/ListToJsonTypeHandler.java
+++ src/main/java/com/yy/framework/flowable/handler/ListToJsonTypeHandler.java
@@ -1,4 +1,4 @@
-package com.yy.ppm.flowable.handler;
+package com.yy.framework.flowable.handler;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
